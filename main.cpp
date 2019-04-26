8af92d05a71dc633406743b9b83e027e630cecf0
Roberto Stagi <r.stagi96@gmail.com>
2019-04-26 13:05:01 +0200

    log in cout ed eccezioni

--- main.cpp
+++ main.cpp
@@ -4,31 +4,39 @@
 #include <iostream>
 #include "PolitoceanExceptions.hpp"
 
+#define MAX_TRIES 10
+
 using namespace std;
 using namespace Politocean;
 
 int main(int argc, const char *argv[])
 {
     JoystickPublisher publisher(JoystickPublisher::DFLT_ADDRESS, JoystickPublisher::DFLT_CLIENT_ID);
+    bool connected = false;
 
     try {
         Joystick joystick;
 
         cout << "Connecting to the publisher..." << endl;
-        try{
-            publisher.connect();
-        }
-        catch(Politocean::mqttException& e){
-            cout << e.what() << endl;
-            exit(EXIT_FAILURE);
+        int n_tries = 0;
+        while (!connected && n_tries < MAX_TRIES){
+            ++n_tries;
+            cout << "Attempt " << n_tries << " to connect..." << endl;
+            try{
+                publisher.connect();
+                connected = true;
+            }
+            catch(Politocean::mqttException& e){
+                cout << "MQTT error: " << e.what() << endl;
+                connected = false;
+            }
         }
 
         joystick.startListening(&JoystickPublisher::callback, &publisher)->join();
         publisher.disconnect();
-    } catch (Joystick::JoystickException& e) {
-        cerr << e.what() << endl;
-    } catch (Politocean::mqttException& e) {
-        cerr << e.what() << endl;
+    } catch (std::exception& e) {
+        cout << e.what() << endl;
+        exit(EXIT_FAILURE);
     }
 
     return 0;
