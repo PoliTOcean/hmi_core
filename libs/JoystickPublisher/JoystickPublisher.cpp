7b7b0154f9495cf0377f0d22335ee29393fd80fc
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2019-05-01 12:02:51 +0200

    Removed stupid std::cout for debug

--- libs/JoystickPublisher/JoystickPublisher.cpp
+++ libs/JoystickPublisher/JoystickPublisher.cpp
@@ -15,14 +15,11 @@ const string JoystickPublisher::DFLT_CLIENT_ID  { "JoystickPublisher" };
 const string JoystickPublisher::DFLT_TOPIC      { "JoystickTopic" };
 
 void JoystickPublisher::callback(vector<int>& axes, vector<int>& buttons) {
-
     map<string, vector<int>> c_map { {"axes", axes}, {"buttons", buttons} };
 
     nlohmann::json j_map(c_map);
 
     this->publish(DFLT_TOPIC, j_map.dump());
-
-    cout << j_map << endl;
 }
 
 }
