22f409d0383fe89f3cf1697330b38a383b3ec338
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2019-05-05 16:51:15 +0200

    Updated Joystick
    New method for connect and isConnected check

--- libs/Joystick/Joystick.h
+++ libs/Joystick/Joystick.h
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <thread>
+#include <chrono>
 #include <exception>
 #include <functional>
 
@@ -17,17 +18,26 @@ namespace Politocean {
 
 class Joystick
 {
+    std::string device_;
+
     int fd, num_of_axes, num_of_buttons;
     char name_of_joystick[80];
     struct js_event js;
 
-    bool _isListening;
+    bool isReading_, isConnected_;
 
     /**
      * @axes and @buttons maps store respectively values for joystick axes and buttons.
      */
-    std::vector<int> axes;
-    unsigned char button;
+    std::vector<int> axes_;
+    unsigned char button_;
+
+    /*
+     * Reads data from joystick and stores them.
+     */
+    void readData();
+
+    std::thread *readingThread_;
 
 public:
     static const std::string DFLT_DEVICE;
@@ -36,24 +46,24 @@ public:
      * It throws a @JoystickException if the open fails.
      */
 
-    Joystick();
-    explicit Joystick(const std::string& device);
+    Joystick() : Joystick(DFLT_DEVICE) {}
+    
+    Joystick(const std::string& device)
+        : device_(device), num_of_axes(0), num_of_buttons(0), isReading_(false), isConnected_(false), button_(0), readingThread_(nullptr) {}
     /**
      * Closes the joystick file descriptor @fd.
      */
     ~Joystick();
 
-    /*
-     * Reads data from joystick and stores them.
-     */
-    void readData();
+    void connect();
+
     /**
      * Returns a thread which is listening to the joystick.
      * @fp is a pointer to the method function
      * @obj is the pointer to the instance object
      */
     template<class M, class T>
-    std::thread* startListening(void (T::*fp)(const std::vector<int>& axes, unsigned char button), M *obj)
+    void startReading(void (T::*fp)(const std::vector<int>& axes, unsigned char button), M *obj)
     {
         if (isListening())
             return nullptr;
@@ -62,44 +72,29 @@ public:
 
         auto callbackFunction = std::bind(fp, obj, std::placeholders::_1, std::placeholders::_2);
 
-        return new std::thread([this, callbackFunction]() {
-            while (isListening())
+        readingThread_ = std::thread([this, callbackFunction]() {
+            while (isReading_)
             {
                 readData();
-                
-                callbackFunction(axes, button);
+                callbackFunction(axes_, button_);
             }
         });
     }
     /**
      * It stops the listening thread by setting @_isListening to false.
      */
-    void stopListening();
+    void stopReading();
 
     /**
      * Returns the value respectively for the @axis axis and @button button inside the maps.
      */
     int getAxis(int axis);
-    bool getButton(int button);
+    unsigned char getButton();
 
-    /*
-     * Return true if there is a thread listening to the joystick, false otherwise.
-     */
-    bool isListening();
-
-    /*
-     * A class that inherits from std::exception, to handle joystick exceptions
-     */
-    class JoystickException : public std::exception {
-        std::string msg;
-
-    public:
-        explicit JoystickException(const std::string &msg) : msg(msg) {}
-
-        const char* what() const noexcept override {
-            return msg.c_str();
-        }
-    };
+    // Returns true is the thread is reading for joystick values
+    bool isReading();
+    // Returns true if the joystick device is connected
+    bool isConnected();
 };
 
 }
