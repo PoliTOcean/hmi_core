21f1ec25a36d63181ebed31e473cbc8f3d796724
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2019-05-05 17:34:21 +0200

    Fixes

--- src/JoystickPublisher.cpp
+++ src/JoystickPublisher.cpp
@@ -7,8 +7,11 @@
 #include "Publisher.h"
 #include "Joystick.h"
 
-#include "PolitoceanExceptions.h"
+#include "PolitoceanExceptions.hpp"
 #include "PolitoceanConstants.h"
+
+#include "logger.h"
+#include "mqttLogger.h"
 
 #include "json.hpp"
 
@@ -22,16 +25,11 @@ class Listener {
 	std::vector<int> axes_;
 	unsigned char button_;
 
-	Joystick joystick_;
 public:
-	Listener(const Joystick& joystick) : joystick_(joystick) {}
-
 	void listen(const std::vector<int>& axes, unsigned char button);
 
 	std::vector<int> axes();
 	unsigned char button();
-
-	void setJoystick(const Joystick& joystick);
 };
 
 void Listener::listen(const std::vector<int>& axes, unsigned char button)
@@ -50,8 +48,6 @@ unsigned char Listener::button()
 	return button_;
 }
 
-
-
 /**************************************************************
  * Talker class for Joystick publisher
  *************************************************************/
@@ -69,21 +65,21 @@ class Talker {
 	bool isTalking_;
 
 public:
-	void startTalking(const Publisher& publisher, const Listener& listener);
+	void startTalking(Publisher& publisher, Listener& listener);
 	void stopTalking();
 
 	bool isTalking();
-}
+};
 
-void Talker::startTalking(const Publisher& publisher, const Listener& listener)
+void Talker::startTalking(Publisher& publisher, Listener& listener)
 {
 	if (isTalking_)
 		return ;
 
 	isTalking_ = true;
 
-	axesTalker = new std::thread([publisher, listener]() {
-		while (publisher.isConnected() && listener.joystick.isConnected())
+	axesTalker_ = new std::thread([&]() {
+		while (publisher.is_connected())
 		{
 			nlohmann::json j_map = { listener.axes() };
 
@@ -93,8 +89,8 @@ void Talker::startTalking(const Publisher& publisher, const Listener& listener)
 		isTalking_ = false;
 	});
 
-	buttonTalker = new std::thread([publisher, listener]() {
-		while (publisher.isConnected() && listener.joystick.isConnected)
+	buttonTalker_ = new std::thread([&]() {
+		while (publisher.is_connected())
 
 			publisher.publish(Constants::Topics::JOYSTICK_BUTTONS, std::to_string(listener.button()));
 		isTalking_ = false;
@@ -107,7 +103,7 @@ void Talker::stopTalking()
 		return ;
 
 	isTalking_ = false;
-	axesTalker->join(); buttonTalker->join();
+	axesTalker_->join(); buttonTalker_->join();
 }
 
 bool Talker::isTalking()
@@ -122,13 +118,13 @@ bool Talker::isTalking()
 #define DFLT_CLIENT_ID "JoystickPublisher"
 #define MAX_JOYSTICK_CONNECTION_RETRY 5
 
-Publisher pub(Hmi::IP_ADDRESS, Hmi::CLIENT_ID);
+Publisher pub(Constants::Hmi::IP_ADDRESS, Constants::Hmi::CLIENT_ID);
 mqttLogger ptoLogger(&pub);
 
 void testcb(const std::string& payload){
     cout << payload << endl;
 }
-Subscriber sub(Hmi::IP_ADDRESS, "testhmi");
+Subscriber sub(Constants::Hmi::IP_ADDRESS, "testhmi");
 
 int main(int argc, const char *argv[])
 {
@@ -137,12 +133,12 @@ int main(int argc, const char *argv[])
     logger::enableLevel(logger::DEBUG, true);
 
 	// Create a publisher object and a talker.
-	Publisher joystickPublisher(Hmi::IP_ADDRESS, DFLT_CLIENT_ID);
+	Publisher joystickPublisher(Constants::Hmi::IP_ADDRESS, DFLT_CLIENT_ID);
 	Talker talker;
 	
 	// Create a joystick object and a listener.
 	Joystick joystick;
-	Listener listener(joystick);
+	Listener listener;
 
 	// Try to connect to the joystick device.
 	// If error has caught, terminate with EXIT_FAILURE
@@ -159,14 +155,14 @@ int main(int argc, const char *argv[])
 	// Start reading data from the joystick device.
 	joystick.startReading(&Listener::listen, &listener);
 	// Start talker talking
-	talker.startTalking(&publisher, &listener);
+	talker.startTalking(joystickPublisher, listener);
 
 	int nretry = 0;
-	while (joystickPublisher.isConnected())
+	while (joystickPublisher.is_connected())
 	{
-		while (joystick.isConnected() && joystickPublisher.isConnected());
+		while (joystick.isConnected() && joystickPublisher.is_connected());
 		
-		if (!joystickPublisher.isConnected())
+		if (!joystickPublisher.is_connected())
 			break;
 		
 		std::cerr << "Joystick device disconnected" << std::endl;
