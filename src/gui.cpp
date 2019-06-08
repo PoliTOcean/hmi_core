197acd17eee472791d58b3703ad70c015f7f8870
Roberto Stagi <r.stagi96@gmail.com>
2019-06-08 11:17:57 +0200

    added automatic launch of Joystick and Commands from GUI

--- src/gui.cpp
+++ src/gui.cpp
@@ -8,6 +8,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <thread>
+#include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 using namespace Politocean;
@@ -16,6 +18,16 @@ using namespace Politocean::Constants;
 int main(int argc, char *argv[])
 {
     logger::enableLevel(logger::DEBUG);
+
+    pid_t pid1, pid2;
+    char *args[]={"",NULL};
+    pid1 = fork();
+    if (pid1 == 0)
+        execvp(args[0]="PolitoceanJoystick",args);
+    
+    pid2 = fork();
+    if (pid2 == 0)
+        execvp(args[0]="PolitoceanCommands",args);
 
     MqttClient& subscriber = MqttClient::getInstance(Hmi::GUI_ID, Hmi::IP_ADDRESS);
 
@@ -27,5 +39,10 @@ int main(int argc, char *argv[])
     subscriber.subscribeTo(Topics::LOGS+"#", &MainWindow::messageArrived, &gui);
     subscriber.subscribeTo(Topics::COMPONENTS, &MainWindow::messageArrived, &gui);
 
-    return a.exec();
+    int result = a.exec();
+
+    kill(pid2, SIGTERM);
+    kill(pid1, SIGTERM);
+
+    return result;
 }
