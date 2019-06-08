7790fc611ad61c14a0d54b21cd8341c761d7f78c
Lorenzo Martellone <lorenzo.martellone98@gmail.com>
2019-06-08 16:51:14 +0200

    Updated

--- src/Mouse.cpp
+++ src/Mouse.cpp
@@ -1,10 +1,29 @@
 #include <X11/Xlib.h>
 #include <iostream>
-#include </usr/include/unistd.h>
+#include <queue>
+#include <thread>
+#include <chrono>
+
 #include <json.hpp>
+
 #include "MqttClient.h"
 #include "PolitoceanUtils.hpp"
 
+#define X_MOUSE 9
+#define Y_MOUSE 10
+
+typedef struct button_t
+{
+    int id;
+    unsigned int value;
+
+    button_t(int id, int value) : id(id), value(value) {}
+
+    bool operator==(const button_t &o) const {
+        return id == o.id && value == o.value;
+    }
+
+} button_t;
 
 class Listener {
     std::queue<button_t> buttons_;
@@ -13,9 +32,6 @@ class Listener {
     bool isAxesUpdated_ = false;
 
     std::vector<int> axes_;
-
-    std::mutex mutexAxs_, mutexBtn_;
-
 
 public:
     void listenForButtons(const std::string& payload);
@@ -29,19 +45,26 @@ public:
     bool isAxesUpdated();
 };
 
+void Listener::listenForAxes(const std::string& payload)
+{
+    auto c_map = nlohmann::json::parse(payload);
+    std::vector<int> tmp = c_map.get<std::vector<int>>();
+	axes_ = {tmp[9], tmp[10]};
+	
+    isAxesUpdated_ = true;
+}
+
 void Listener::listenForButtons(const std::string& payload)
 {
-	std::lock_guard<std::mutex> lock(mutexBtn_);
-    int btn  = static_cast<int>(std::stoi(payload));
+    int btn = static_cast<int>(std::stoi(payload));
 
-    buttons_.push( button_t(btn & 0x7F, (btn >> 7) & 0x01) );
+    buttons_.push(button_t(btn & 0x7F, (btn >> 7) & 0x01));
 
     isButtonUpdated_ = true;
 }
 
 button_t Listener::button()
 {
-   	std::lock_guard<std::mutex> lock(mutexBtn_);
     if (!buttons_.empty())
     {
         button_t button = buttons_.front();
@@ -61,32 +84,19 @@ bool Listener::isAxesUpdated()
     return !axes_.empty();
 }
 
-void Listener::listenForAxes(const std::string& payload)
-{
-   	std::lock_guard<std::mutex> lock(mutexAxs_);
-    auto c_map = nlohmann::json::parse(payload);
-    std::vector<int> tmp = c_map.get<std::vector<int>>();
-	axes_ = {tmp[9], tmp[10]};
-	
-    isAxesUpdated_ = true;
-}
-
 std::vector<int> Listener::axes(){
-   	std::lock_guard<std::mutex> lock(mutexAxs_);
     isAxesUpdated_ = false;
     return axes_;
 }
+
 int main(void) {
 	
 	MqttClient& subscriber = MqttClient::getInstance(Rov::ATMEGA_ID, Rov::IP_ADDRESS);
 	
 	Listener listener;
 
-	// Subscribe @subscriber to joystick publisher topics 9 10 bott 30
 	subscriber.subscribeTo(Topics::JOYSTICK_BUTTONS, &Listener::listenForButtons, &listener);
     subscriber.subscribeTo(Topics::JOYSTICK_AXES, &Listener::listenForAxes, &listener);
-
-
 
     Display* dpy = XOpenDisplay(0);
     int scr = XDefaultScreen(dpy);
@@ -94,18 +104,20 @@ int main(void) {
 
     int height = DisplayHeight(dpy, scr);
     int width  = DisplayWidth(dpy, scr);
-	std::vector<int> contr;
+	std::cout << "Screen size : x: " << width << "\thegiht: " << height << std::endl;
 	
-	while(1){
+	std::vector<int> contr;
+	while(1)
+	{
 		contr = listener.axes();
-		contr[0] = Politocean::map(contr[0], SHRT_MIN, SHRT_MAX, 0, widht);
-		contr[1] = Politocean::map(contr[1], SHRT_MIN, SHRT_MAX, 0, height);
-		std::cout << "Screen size : " << width << "x" << height << std::endl;
+		contr[X_MOUSE] = Politocean::map(contr[X_MOUSE], SHRT_MIN, SHRT_MAX, 0, widht);
+		contr[Y_MOUSE] = Politocean::map(contr[Y_MOUSE], SHRT_MIN, SHRT_MAX, 0, height);
 
 
 		XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, contr[0], contr[1]);
 		XFlush(dpy);
-		usleep(50);
+		
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
 	}
 }
 
