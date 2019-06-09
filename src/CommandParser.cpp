6b5a6485b8433ed6180dc6009c2f5e9e7aa525be
pettinz <angelo.pettinelli@icloud.com>
2019-06-10 00:21:14 +0200

    Updated button for reflectables

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -14,32 +14,21 @@
 #include "PolitoceanExceptions.hpp"
 #include "mqttLogger.h"
 
-#include <Reflectables/Vector.hpp>
+#include "Reflectables/Vector.hpp"
 
 #include "ComponentsManager.hpp"
+#include "Button.hpp"
 
 using namespace Politocean;
 using namespace Politocean::Constants;
 using namespace Politocean::Constants::Commands;
+
 /**************************************************************
  * Listener class for Joystick device
  *************************************************************/
 
-typedef struct button_t
-{
-    int id;
-    unsigned int value;
-
-    button_t(int id, int value) : id(id), value(value) {}
-
-    bool operator==(const button_t &o) const {
-        return id == o.id && value == o.value;
-    }
-
-} button_t;
-
 class Listener {
-    std::queue<button_t> buttons_;
+    std::queue<Button> buttons_;
 
     bool isButtonUpdated_ = false;
     bool isAxesUpdated_ = false;
@@ -49,10 +38,10 @@ class Listener {
     std::mutex mutexBtn_;
 
 public:
-    void listenForButtons(const std::string& payload);
+    void listenForButtons(Button button);
     void listenForAxes(const std::string& payload);
 
-    button_t button();
+    Button button();
 
     Types::Vector<int> axes();
 
@@ -60,31 +49,24 @@ public:
     bool isAxesUpdated();
 };
 
-void Listener::listenForButtons(const std::string& payload)
+void Listener::listenForButtons(Button button)
 {
-	std::lock_guard<std::mutex> lock(mutexBtn_);
-
-    int btn = 0;
-    try {
-        btn  = static_cast<int>(std::stoi(payload));
-    } catch(...) {
-        logger::getInstance().log(logger::WARNING, "Error in receiving buttons");
-    }
-    buttons_.push( button_t(btn & 0x7F, (btn >> 7) & 0x01) );
+    buttons_.push(button);
 
     isButtonUpdated_ = true;
 }
 
-button_t Listener::button()
+Button Listener::button()
 {
    	std::lock_guard<std::mutex> lock(mutexBtn_);
-    if (!buttons_.empty())
-    {
-        button_t button = buttons_.front();
-        buttons_.pop();
-        return button;
-    }
-    return button_t(-1, 0);
+    
+    if (buttons_.empty())
+        return Button(-1, 0);
+    
+    Button button = buttons_.front();
+    buttons_.pop();
+    
+    return button;
 }
 
 bool Listener::isButtonUpdated()
@@ -134,6 +116,7 @@ public:
 
 void Talker::startTalking(MqttClient& publisher, Listener& listener)
 {
+
     if (isTalking_)
         return ;
 
@@ -217,9 +200,9 @@ void Talker::startTalking(MqttClient& publisher, Listener& listener)
                 continue;
             }
             
-            button_t button = listener.button();
-            int id      = button.id;
-            int value   = button.value;
+            Button button   = listener.button();
+            int id          = button.getId();
+            int value       = button.getValue();
             
 
             string action = Actions::NONE;
@@ -400,7 +383,7 @@ bool Talker::isTalking()
 
 int main(int argc, const char* argv[])
 {
-    logger::enableLevel(logger::DEBUG);
+    logger::enableLevel(logger::INFO);
 
     MqttClient& hmiClient = MqttClient::getInstance(Constants::Hmi::CMD_ID, Constants::Hmi::IP_ADDRESS);
     Listener listener;
